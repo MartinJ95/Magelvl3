bd929779718697964f1ed149cd8c3d635fd688d7
MartinJ95 <martinjones95@gmx.co.uk>
2025-01-09 13:16:25 +0000

    feat vector : initial implementation

---
+++ Utilities/include/Vector.h
@@ -0,0 +1,14 @@
+#pragma once
+
+struct Vector3
+{
+public:
+	Vector3() : x(0), y(0), z(0) {}
+	Vector3(float X, float Y, float Z) : x(X), y(Y), z(Z) {}
+public:
+	void operator=(const Vector3& rhs) { x = rhs.x; y = rhs.y; z = rhs.z; };
+	void operator*=(const float s) { x *= s; y *= s; z *= s; };
+	bool operator==(const Vector3& rhs) { return x == rhs.x && y == rhs.y && z == rhs.z; };
+public:
+	float x, y, z;
+};
