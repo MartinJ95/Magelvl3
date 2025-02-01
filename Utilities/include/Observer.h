15427b521e99073ff5e4bfc8bcfcb8c2b5396e51
MartinJ95 <martinjones95@gmx.co.uk>
2025-02-01 18:11:20 +0000

    feat observer pattern : implementation

---
+++ Utilities/include/Observer.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <vector>
+
+template<typename T>
+class Subject;
+
+template<typename T>
+class Observer
+{
+public:
+	Observer() : m_subject(nullptr)
+	{
+
+	}
+	void AddSubject(Subject<T>* NewSubject)
+	{
+		m_subject = NewSubject;
+	}
+	virtual void OnNotify(const T&) = 0;
+private:
+	Subject<T>* m_subject;
+};
+
+template<typename T>
+class Subject
+{
+public:
+	virtual void Notify(const T& Data)
+	{
+		for (int i = 0; i < m_observers.size(); i++)
+		{
+			if (m_observers.at(i) == nullptr)
+			{
+				continue;
+			}
+			m_observers.at(i)->OnNotify(Data);
+		}
+		/*
+		for (auto& it : m_observers)
+		{
+			if (it == nullptr)
+			{
+				continue;
+			}
+			it->OnNotify(Data);
+			
+		}
+		*/
+	}
+	void AddSubscriber(Observer<T>* NewObserver)
+	{
+		m_observers.emplace_back(NewObserver);
+		NewObserver->AddSubject(this);
+	}
+private:
+	std::vector<Observer<T>*> m_observers;
+};
