3014f7da846edefdbfcddd03e51c09039949499c
MartinJ95 <martinjones95@gmx.co.uk>
2025-02-01 18:16:06 +0000

    feat component : initial implementation

--- Utilities/include/Component.h
+++ Utilities/include/Component.h
@@ -1,1 +1,10 @@
 #pragma once
+
+class Component
+{
+public:
+	virtual void BeginPlay() = 0;
+	virtual void Update(float DeltaTime) = 0;
+	virtual void OnDestroy() = 0;
+	virtual void OnInput(const int Key, const int Scancode, const int Action, const int Mods) {}
+};
