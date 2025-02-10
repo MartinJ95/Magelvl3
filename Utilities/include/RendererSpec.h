285f886bce137c76c1a593c3f91f4f4c2c141990
MartinJ95 <martinjones95@gmx.co.uk>
2025-02-10 13:08:31 +0000

    feat renderer spec
    this commit should define a base class for renderers to help with creating the class for other platforms

---
+++ Utilities/include/RendererSpec.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Vector.h"
+
+class RendererSpec
+{
+public:
+	virtual void AddToRenderQueue(const unsigned int RenderPass, const Vector3 Pos) = 0;
+	virtual void PositionCamera(const Vector3& Position, const Vector3& Rotation) = 0;
+	virtual void Render() = 0;
+	virtual bool WindowShouldClose() const = 0;
+	virtual void PollEvents() = 0;
+};
