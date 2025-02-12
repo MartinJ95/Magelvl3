288c78b86598cc02bf455413dc46fd4fa5ba1391
MartinJ95 <martinjones95@gmx.co.uk>
2025-02-12 15:49:32 +0000

    vector operators
    implemented following operators to vec 3's
    
    +
    *
    ==
    !=

--- Utilities/include/Vector.h
+++ Utilities/include/Vector.h
@@ -7,12 +7,12 @@ public:
 	Vector3(float X, float Y, float Z) : x(X), y(Y), z(Z) {}
 public:
 	void operator=(const Vector3& rhs) { x = rhs.x; y = rhs.y; z = rhs.z; };
-	Vector3 operator*(const float& s) { return Vector3(x * s, y * s, z * s); }
-	Vector3 operator+(const Vector3& rhs) { return Vector3(x + rhs.x, y + rhs.y, z + rhs.z); }
+	Vector3 operator*(const float& s) const { return Vector3(x * s, y * s, z * s); }
+	Vector3 operator+(const Vector3& rhs) const { return Vector3(x + rhs.x, y + rhs.y, z + rhs.z); }
 	void operator*=(const float s) { x *= s; y *= s; z *= s; };
 	void operator+=(const Vector3& rhs) { x += rhs.x; y += rhs.y; z += rhs.z; };
-	bool operator==(const Vector3& rhs) { return x == rhs.x && y == rhs.y && z == rhs.z; };
-	bool operator!=(const Vector3& rhs) { return !(*this == rhs); }
+	bool operator==(const Vector3& rhs) const { return x == rhs.x && y == rhs.y && z == rhs.z; };
+	bool operator!=(const Vector3& rhs) const { return !(*this == rhs); }
 public:
 	float x, y, z;
 };
